2f904289d314f16218771f8b5ea1ba40912c75c2
DarkLord76865 <mmima191@gmail.com>
2023-12-20 23:17:59 +0100

    update 1.0.3

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import pyperclip
+import base64
+import os
+import sys
 import tkinter as tk
 from tkinter.messagebox import showinfo, showerror
-import sys
-import os
-import base64
+
+import pyperclip
 
 
 def resource_path(relative_path):
@@ -23,74 +24,75 @@ def gen_link(link):
 	else:
 		return ""
 
-def gen_click(event):
-	global link_entry, root
-	generated_link = gen_link(link_entry.get())
 
-	if len(generated_link) == 0:
-		showerror(title="Error!", message="Generating direct download link failed!\nCheck link and try again!", parent=root)
-	else:
-		pyperclip.copy(generated_link)
-		showinfo(title="Success!", message="Direct download link copied to clipboard!", parent=root)
+class App:
+	def __init__(self):
+		self.root = tk.Tk()
+		self.root.title("Direct-Link")
+		self.root.geometry(f"800x190"
+		                   f"+{self.root.winfo_screenwidth() // 2 - 400}"
+		                   f"+{self.root.winfo_screenheight() // 2 - 95}")
+		self.root.config(background="#FFC0CB")
+		self.root.resizable(False, False)
+		self.root.iconbitmap(resource_path("resources/download-icon.ico"))
 
-def paste_click(event):
-	global link_entry
-	link_entry.delete(0, tk.END)
-	link_entry.insert(0, pyperclip.paste())
+		self.title = tk.Label(self.root, text="Direct-Link", font=("Gabriola", 75, "italic", "bold"),
+		                      foreground="white", activeforeground="white",
+		                      background="#FFC0CB", activebackground="#FFC0CB",
+		                      highlightthickness=0, borderwidth=0)
+		self.title.place(x=0, y=7, width=750, height=100)
 
-def gui():
-	global link_entry, root
+		self.link_label = tk.Label(self.root, text="LINK:", font=("Gabriola", 30, "bold"),
+		                           foreground="white", activeforeground="white",
+		                           background="#FFC0CB", activebackground="#FFC0CB",
+		                           highlightthickness=0, borderwidth=0)
+		self.link_label.place(x=0, y=125, width=100, height=35)
 
-	root = tk.Tk()
-	root.title("Direct-Link")
-	root.geometry(f"800x190+{root.winfo_screenwidth() // 2 - 400}+{root.winfo_screenheight() // 2 - 95}")
-	root.config(background="#FFC0CB")
-	root.resizable(False, False)
-	root.iconbitmap(resource_path("data/download-icon.ico"))
+		self.link_entry = tk.Entry(self.root, font=("Helvetica", 11), justify=tk.CENTER, insertbackground="white",
+		                           foreground="white", disabledforeground="white",
+		                           background="#E5ACB6", disabledbackground="#E5ACB6",
+		                           highlightthickness=2, highlightcolor="white", highlightbackground="white",
+		                           borderwidth=0)
+		self.link_entry.place(x=100, y=125, width=500, height=35)
 
-	title = tk.Label(text="Direct-Link", font=("Gabriola", 75, "italic", "bold"),
-	                 foreground="white", activeforeground="white",
-	                 background="#FFC0CB", activebackground="#FFC0CB",
-	                 highlightthickness=0, borderwidth=0)
-	title.place(x=0, y=7, width=750, height=100)
+		self.paste_btn = tk.Label(self.root, text="Paste", font=("Gabriola", 25, "bold"), cursor="hand2",
+		                          foreground="white", activeforeground="white",
+		                          background="#FFCCD5", activebackground="#FFCCD5",
+		                          highlightthickness=3, highlightcolor="white", highlightbackground="white",
+		                          borderwidth=0)
+		self.paste_btn.place(x=600, y=125, width=75, height=35)
+		self.paste_btn.bind("<Enter>", lambda event: self.paste_btn.config(background="#FFD9DF", activebackground="#FFD9DF"))
+		self.paste_btn.bind("<Leave>", lambda event: self.paste_btn.config(background="#FFCCD5", activebackground="#FFCCD5"))
+		self.paste_btn.bind("<ButtonRelease-1>", lambda event: self.paste_click())
 
-	link_label = tk.Label(text="LINK:", font=("Gabriola", 30, "bold"),
-	                      foreground="white", activeforeground="white",
-	                      background="#FFC0CB", activebackground="#FFC0CB",
-	                      highlightthickness=0, borderwidth=0)
-	link_label.place(x=0, y=125, width=100, height=35)
+		self.gen_btn = tk.Label(self.root, text="Generate", font=("Gabriola", 25, "bold"), cursor="hand2",
+		                        foreground="white", activeforeground="white",
+		                        background="#FFCCD5", activebackground="#FFCCD5",
+		                        highlightthickness=3, highlightcolor="white", highlightbackground="white",
+		                        borderwidth=0)
+		self.gen_btn.place(x=675, y=125, width=125, height=35)
+		self.gen_btn.bind("<Enter>", lambda event: self.gen_btn.config(background="#FFD9DF", activebackground="#FFD9DF"))
+		self.gen_btn.bind("<Leave>", lambda event: self.gen_btn.config(background="#FFCCD5", activebackground="#FFCCD5"))
+		self.gen_btn.bind("<ButtonRelease-1>", lambda event: self.gen_click())
 
-	link_entry = tk.Entry(font=("Helvetica", 11), justify=tk.CENTER, insertbackground="white",
-	                      foreground="white", disabledforeground="white",
-	                      background="#E5ACB6", disabledbackground="#E5ACB6",
-	                      highlightthickness=2, highlightcolor="white", highlightbackground="white",
-	                      borderwidth=0)
-	link_entry.place(x=100, y=125, width=500, height=35)
+		self.root.mainloop()
 
-	paste_btn = tk.Label(text="Paste", font=("Gabriola", 25, "bold"),
-	                     foreground="white", activeforeground="white",
-	                     background="#FFCCD5", activebackground="#FFCCD5",
-	                     highlightthickness=3, highlightcolor="white", highlightbackground="white",
-	                     borderwidth=0)
-	paste_btn.place(x=600, y=125, width=75, height=35)
-	paste_btn.bind("<Enter>", lambda event: paste_btn.config(background="#FFD9DF", activebackground="#FFD9DF"))
-	paste_btn.bind("<Leave>", lambda event: paste_btn.config(background="#FFCCD5", activebackground="#FFCCD5"))
-	paste_btn.bind("<ButtonRelease-1>", paste_click)
+	def paste_click(self):
+		self.link_entry.delete(0, tk.END)
+		self.link_entry.insert(0, pyperclip.paste())
 
-	gen_btn = tk.Label(text="Generate", font=("Gabriola", 25, "bold"),
-	                   foreground="white", activeforeground="white",
-	                   background="#FFCCD5", activebackground="#FFCCD5",
-	                   highlightthickness=3, highlightcolor="white", highlightbackground="white",
-	                   borderwidth=0)
-	gen_btn.place(x=675, y=125, width=125, height=35)
-	gen_btn.bind("<Enter>", lambda event: gen_btn.config(background="#FFD9DF", activebackground="#FFD9DF"))
-	gen_btn.bind("<Leave>", lambda event: gen_btn.config(background="#FFCCD5", activebackground="#FFCCD5"))
-	gen_btn.bind("<ButtonRelease-1>", gen_click)
+	def gen_click(self):
+		generated_link = gen_link(self.link_entry.get())
 
-	root.mainloop()
+		if len(generated_link) == 0:
+			showerror(title="Error!", message="Generating direct download link failed!\nCheck link and try again!", parent=self.root)
+		else:
+			pyperclip.copy(generated_link)
+			showinfo(title="Success!", message="Direct download link copied to clipboard!", parent=self.root)
+
 
 def main():
-	gui()
+	App()
 
 
 if __name__ == "__main__":
